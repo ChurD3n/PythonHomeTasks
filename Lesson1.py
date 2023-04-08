32bea3861155404823633f65962f11e3606352ce
Denis <demid200479@gmail.com>
2023-04-08 17:50:25 +0300

    First lesson

---
+++ Lesson1.py
@@ -0,0 +1,31 @@
+# задача про путь в км 
+"""print('Enter the distance:')
+n = int(input())
+print('Enter km per day:')
+m = int(input())
+print((n-1)//(m+1)))"""
+#Задача про парты
+"""
+a1 = int(input("enter 1:"))
+a2 = int(input("enter 2:"))
+a3 = int(input("enter 3:"))
+c = int((a1/2 + a2/2 +a3/2))
+print(c)
+"""
+#Петя и вагоны
+"""
+i = int(input('введите номер вагона по счету '))
+j = int(input('введите номер вагона, написанный в вагоне:'))
+if i<j:
+    print(f'кол-во вагонов равно: {i+j-1}')
+else: 
+    print('Недостаточно информации')
+"""
+#Високосный год
+"""
+year = int(input('Введите год '))
+if (year%4 ==0 or year%100 != 0) and year%400 ==0:
+    print('високосный')
+else: 
+    print('не високосный')
+"""
